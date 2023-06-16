691f04f5a717c9abccd9be7da23f5b1d4db75fa1
Les <letsohyeah@gmail.com>
2023-06-16 11:53:49 +0800

    [feat] 調整 ACF flexible thumbnail自訂圖片路徑

--- wordpress/wp-content/themes/es-nuxt/functions.php
+++ wordpress/wp-content/themes/es-nuxt/functions.php
@@ -27,45 +27,45 @@ include_once "api/index.php";
 
 add_filter('acfe/flexible/thumbnail/layout=full_cards', 'full_cards_thumbnail', 10, 3);
 function full_cards_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/full_cards.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=ani_number', 'ani_number_thumbnail', 10, 3);
 function ani_number_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/ani_number.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=ig_show', 'ig_show_thumbnail', 10, 3);
 function ig_show_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/ig_show.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=room_show', 'room_show_thumbnail', 10, 3);
 function room_show_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/room_show.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=big_slider', 'big_slider_thumbnail', 10, 3);
 function big_slider_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/big_slider.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=half_layout', 'half_layout_thumbnail', 10, 3);
 function half_layout_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/half_layout.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=full_bg', 'full_bg_thumbnail', 10, 3);
 function full_bg_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/full_bg.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=title_double', 'title_double_thumbnail', 10, 3);
 function title_double_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/title_double.jpg';
 }
 
 add_filter('acfe/flexible/thumbnail/layout=title_big_img', 'title_big_img_thumbnail', 10, 3);
 function title_big_img_thumbnail($thumbnail, $field, $layout){
-    return 'http://localhost:3000/thumbnail/test1.png';
+    return get_template_directory_uri() . '/asset/thumbnail/title_big_img.jpg';
 }
