62c9bff9d295a355a8d48649073c4d3f4a0fcbe5
Les <letsohyeah@gmail.com>
2023-06-20 11:15:46 +0800

    [feat] 新增igShow元件

---
+++ nuxt-app/components/elements/igCard.vue
@@ -0,0 +1,174 @@
+<template>
+    <div class="elements-ig-card">
+        <div class="elements-ig-card__top">
+            <figure>
+                <img
+                    :src="data?.user_img?.url"
+                    :alt="data?.user_img?.alt"
+                />
+            </figure>
+            <p v-text="data?.user_name"></p>
+            <div class="elements-ig-card__top-dot">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <figure>
+            <img
+                :src="data?.image?.url"
+                :alt="data?.image?.alt"
+            />
+        </figure>
+        <div class="elements-ig-card__bottom">
+            <div class="elements-ig-card__bottom-top">
+                <nuxt-icon name="heart" />
+                <nuxt-icon name="comment" />
+                <nuxt-icon name="share" />
+                <nuxt-icon name="save" />
+            </div>
+            <p v-text="`${data.like} likes`"></p>
+            <p v-text="data.text"></p>
+            <p v-text="data.s_text"></p>
+        </div>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        data: {
+            type: Object,
+            default: {},
+        },
+    })
+</script>
+<style lang="scss">
+    $class-name: elements-ig-card;
+    .#{$class-name} {
+        display: flex;
+        flex-direction: column;
+
+        &__top {
+            display: flex;
+            align-items: center;
+            padding: 1.6rem 1.6rem 1.2rem;
+            background-color: map-get($colors, white);
+            color: map-get($colors, black-1);
+
+            > figure {
+                @include size(2.4rem);
+
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                overflow: hidden;
+                border-radius: 50%;
+
+                > img {
+                    @include size(100%);
+
+                    position: absolute;
+                    object-fit: cover
+                }
+            }
+
+            > p {
+                @include typo('small', 12);
+
+                margin-left: 1.2rem;
+            }
+
+            > span {
+                @include size(.2rem);
+
+                display: flex;
+                border-radius: 50%;
+                margin-left: auto;
+                background-color: map-get($colors, black-1);
+
+                &::before, &::after {
+                    @include size(.2rem);
+
+                    content: '';
+
+                    display: flex;
+                    flex: 0 0 auto;
+                    border-radius: 50%;
+                    background-color: map-get($colors, black-1);
+                    margin: 0 0.2rem;
+                }
+            }
+
+            &-dot {
+                display: flex;
+                margin-left: auto;
+
+                > span {
+                    @include size(.2rem);
+
+                    border-radius: 50%;
+                    background-color: map-get($colors, black-1);
+
+                    &:nth-child(2) {
+                        margin: 0 .2rem;
+                    }
+                }
+            }
+        }
+
+        > figure {
+            @include size(100%, auto);
+            @include aspect(1.05);
+
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+
+            > img {
+                @include size(100%);
+
+                position: absolute;
+                object-fit: cover;
+            }
+        }
+
+        &__bottom {
+            color: map-get($colors, black-1);
+            padding: 1.2rem 1.6rem 1.6rem;
+            background-color: map-get($colors, white);
+            margin-bottom: 0.6rem;
+
+            &-top {
+                display: flex;
+                margin-bottom: 0.6rem;
+
+                > span {
+                    &:last-child {
+                        margin-left: auto;
+                    }
+                }
+            }
+
+            > p {
+                @include typo('small', 12);
+                
+                &:not(:last-child) {
+                    margin-bottom: 0.6rem;
+                }
+
+                &:nth-child(3) {
+                    &::after {
+                        content: ' ...more';
+                        color: map-get($colors, gray-2);
+                    }
+                }
+
+                &:last-child {
+                    color: map-get($colors, gray-2);
+                }
+            }
+        }
+    }
+</style>
---
+++ nuxt-app/components/flexible/igShow.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="flexible-ig-show container" :style="{ backgroundImage: 'url(' + data?.bg_img?.url + ')' }">
+        <div class="flexible-ig-show__bg"></div>
+        <div class="flexible-ig-show__wrapper">
+            <div class="flexible-ig-show__wrapper-title">
+                <p v-text="data?.title"></p>
+                <ElementsDecoTitle :data="data?.deco_title"/>
+            </div>
+            <div class="flexible-ig-show__wrapper-ig">
+                <div
+                    class="flexible-ig-show__wrapper-ig-wrapper"
+                    v-for="(card, key) in data?.ig_cards"
+                    :key="key"
+                >
+                    <ElementsIgCard :data="card" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        data: {
+            type: Object,
+            default: {},
+        },
+    })
+</script>
+<style lang="scss">
+    $class-name: flexible-ig-show;
+    .#{$class-name} {
+        @include size(100%, auto);
+
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-top: 20rem;
+        padding-bottom: 12rem;
+        color: map-get($colors, white);
+        background-attachment: fixed;
+
+        &__bg {
+            @include size(100%);
+
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+
+            &::after {
+                @include size(100%);
+
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                background-color: rgba(#484036, .4);
+            }
+        }
+
+        &__wrapper {
+            @include set-col(10, 12, 0);
+
+            display: flex;
+            flex-direction: column;
+            z-index: 1;
+
+            &-title {
+                display: flex;
+                flex-direction: column;
+                margin-bottom: 10.8rem;
+
+                > p {
+                    @include typo('heading', 32);
+
+                    margin-bottom: 2rem;
+                }
+            }
+
+            &-ig {
+                @include size(100%, auto);
+
+                display: flex;
+                flex-wrap: wrap;
+
+                &-wrapper {
+                    @include set-col(3, 10, 1);
+
+                    margin-bottom: 4.8rem;
+
+                    &:nth-child(3n + 1) {
+                        padding-top: 11.8rem;
+                    }
+
+                    &:nth-child(3n + 2) {
+                        padding-top: 26.4rem;
+                    }
+
+                    &:not(:nth-child(3)) {
+                        margin-right: 4.5rem;
+                    }
+
+                }
+            }
+        }
+    }
+</style>
