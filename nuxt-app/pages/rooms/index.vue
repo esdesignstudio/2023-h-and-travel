0bf42f9522c39fcc7aea6ac8c5e2428cceab0a4b
melody <melody@e-s.tw>
2023-06-20 18:41:27 +0800

    [feat]  rooms list 頁切版

--- nuxt-app/pages/rooms/index.vue
+++ nuxt-app/pages/rooms/index.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="template">
-
+    <div class="page-rooms">
+        <div class="container">
+            <div class="page-rooms__wrap">
+               <div class="page-rooms__wrap-title">
+                    <h1>房型一覽</h1>
+                    <span>ROOMS</span>
+               </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
@@ -10,10 +17,34 @@
             default: {},
         },
     })
+
+    const pageloaded = usePageLoaded()
+    // if (pageData.value) {
+        pageloaded.value = true
+    // } else {
+    //     navigateTo('/404')
+    // }
 </script>
 <style lang="scss">
-    $class-name: template;
+    $class-name: page-rooms;
     .#{$class-name} {
+        &__wrap {
+            padding: 5rem 0;
 
+            &-title {
+
+                text-align: center;
+
+                h1 {
+                    @include typo('heading', 64);
+
+                    padding: 0 0 1rem;
+                }
+
+                span {
+                    @include typo('display', 140);
+                }
+            }
+        }
     }
 </style>
