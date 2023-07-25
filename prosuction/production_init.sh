995a45d098be7cc0752cc3aa4a39971e554e0e91
Tommy Liu <tommyliu722@hotmail.com>
2023-07-25 16:13:33 +0800

    [feat]production_init.sh加入自動確認

--- prosuction/production_init.sh
+++ prosuction/production_init.sh
@@ -2,7 +2,7 @@
 
 # 安裝docker
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -14,12 +14,12 @@ echo \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
 # 測試docker
 # sudo docker run hello-world
 
 # 安裝nginx
-sudo apt-get install nginx
+sudo apt-get install -y nginx
 sudo ufw allow 'Nginx Full'
 sudo systemctl restart nginx
